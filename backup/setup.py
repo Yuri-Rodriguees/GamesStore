b4130b8a1e34912c3d4260ed55fd2f8ab4083343
Yuri Rodrigues <yurii.devv@gmail.com>
2025-10-19 14:18:53 -0300

    feat: Inicio - v3

--- backup/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-from Cython.Build import cythonize
-
-setup(
-    ext_modules=cythonize(["xcore.py", "uxmod.py", "updater.py", "datax.py"], compiler_directives={'language_level': "3"})
-)
-
